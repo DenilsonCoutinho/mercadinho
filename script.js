99b03b70f89fe5be4edb0b2ce2768d9694059e72
d-e-n-i <denilsoncoutinho9@gmail.com>
2022-04-02 21:11:36 -0300

    att mercadinho

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const openMenu = document.querySelector('.login')
 menu.addEventListener('click', ()=>{
     openMenu.classList.toggle('active')
 })
-const prod = document.querySelector('.active-prod')
-const openProd = document.querySelector('.list')
+
+const prod = document.getElementById('car-active')
+const openProd = document.querySelector('.car')
 prod.addEventListener('click', ()=>{
     openProd.classList.toggle('active')
 })
+
+function addArroz(){
+const arroz = document.getElementById('arroz')
+const addarroz = arroz.children[1].cloneNode(true)
+const arrozPosition = addarroz.children.length + 1
+addarroz.querySelector('.produtos').innerText = "produto" + arrozPosition + ""
+arroz.appendChild(addarroz)
+console.log(arroz.children)
+}
