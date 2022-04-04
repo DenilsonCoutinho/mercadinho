e79e9d06cbfa42535ec6703443c8c25b34c92b97
d-e-n-i <denilsoncoutinho9@gmail.com>
2022-04-04 19:40:57 -0300

    att Mercadinho

--- script.js
+++ script.js
@@ -1,73 +1,80 @@
  const menu = document.querySelector('.active-menu')
-const openMenu = document.querySelector('.login')
-menu.addEventListener('click', ()=>{
-    openMenu.classList.toggle('active')
-})
+ const openMenu = document.querySelector('.login')
+ menu.addEventListener('click', () => {
+     openMenu.classList.toggle('active')
+ })
 
-const prod = document.getElementById('car-active')
-const openProd = document.querySelector('.car')
-prod.addEventListener('click', ()=>{
-    openProd.classList.toggle('active')
-})
+ const prod = document.getElementById('car-active')
+ const openProd = document.querySelector('.car')
+ prod.addEventListener('click', () => {
+     openProd.classList.toggle('active')
+ })
 
-function addArroz(){
-    const buy = document.querySelector('.prod1')
-    const newBuy = buy.children[0].cloneNode(true)
-    const car = document.querySelector('.purchase')
-    car.appendChild(newBuy)
-    
-}
-function addFeijao(){
-    const buy = document.querySelector("#feijao")
-    const newBuy = buy.children[0].cloneNode(true)
-    const car = document.querySelector('.purchase')
-    car.appendChild(newBuy)
-    
-    }
-    function addMacarrao(){
-        const buy = document.querySelector("#macarrao")
-        const newBuy = buy.children[0].cloneNode(true)
-        const car = document.querySelector('.purchase')
-        car.appendChild(newBuy)
-        
-        }
-        function addPringle(){
-              
-            const buy = document.querySelector("#pringle")
-            const newBuy = buy.children[0].cloneNode(true)
-            const car = document.querySelector('.purchase')
-            car.appendChild(newBuy)
-            
-            }
-            function addNutela(){
-              
-                const buy = document.querySelector("#nutela")
-                const newBuy = buy.children[0].cloneNode(true)
-                const car = document.querySelector('.purchase')
-                car.appendChild(newBuy)
-                
-                }
-                function addOreo(){
-              
-                    const buy = document.querySelector("#oreo")
-                    const newBuy = buy.children[0].cloneNode(true)
-                    const car = document.querySelector('.purchase')
-                    car.appendChild(newBuy)
-                    
-                    }
-                    function addClube(){
-              
-                        const buy = document.querySelector("#clube-social")
-                        const newBuy = buy.children[0].cloneNode(true)
-                        const car = document.querySelector('.purchase')
-                        car.appendChild(newBuy)
-                        
-                        }
-                        function addCream(){
-              
-                            const buy = document.querySelector("#creamcrack")
-                            const newBuy = buy.children[0].cloneNode(true)
-                            const car = document.querySelector('.purchase')
-                            car.appendChild(newBuy)
-                            
-                            }
+ function addArroz() {
+     const buy = document.querySelector('.prod1')
+     const newBuy = buy.children[0].cloneNode(true)
+     const car = document.querySelector('.purchase')
+     car.appendChild(newBuy)
+
+ }
+
+ function addFeijao() {
+     const buy = document.querySelector("#feijao")
+     const newBuy = buy.children[0].cloneNode(true)
+     const car = document.querySelector('.purchase')
+     car.appendChild(newBuy)
+
+ }
+
+ function addMacarrao() {
+     const buy = document.querySelector("#macarrao")
+     const newBuy = buy.children[0].cloneNode(true)
+     const car = document.querySelector('.purchase')
+     car.appendChild(newBuy)
+
+ }
+
+ function addPringle() {
+
+     const buy = document.querySelector("#pringle")
+     const newBuy = buy.children[0].cloneNode(true)
+     const car = document.querySelector('.purchase')
+     car.appendChild(newBuy)
+
+ }
+
+ function addNutela() {
+
+     const buy = document.querySelector("#nutela")
+     const newBuy = buy.children[0].cloneNode(true)
+     const car = document.querySelector('.purchase')
+     car.appendChild(newBuy)
+
+ }
+
+ function addOreo() {
+
+     const buy = document.querySelector("#oreo")
+     const newBuy = buy.children[0].cloneNode(true)
+     const car = document.querySelector('.purchase')
+     car.appendChild(newBuy)
+
+ }
+
+ function addClube() {
+
+     const buy = document.querySelector("#clube-social")
+     const newBuy = buy.children[0].cloneNode(true)
+     const car = document.querySelector('.purchase')
+     car.appendChild(newBuy)
+
+ }
+
+ function addCream() {
+
+     const buy = document.querySelector("#creamcrack")
+     const newBuy = buy.children[0].cloneNode(true)
+     const car = document.querySelector('.purchase')
+     car.appendChild(newBuy)
+
+ }
